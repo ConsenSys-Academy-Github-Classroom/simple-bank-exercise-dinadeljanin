2c08d8af7c5fcf722c7502c142e3d14ad3a628ab
dinadeljanin <ddeljanin5@icloud.com>
2021-09-18 18:52:47 -0400

    Replaced natspec, typo fix.

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -18,30 +18,40 @@ contract SimpleBank {
     event LogDepositMade(address accountAddress, uint amount);
     event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
 
-    // After version 6 the recieve function is used instead of the unnamed function
+    // After version 6 the receive function is used instead of the unnamed function
     // But if the receive function isn't implemented, ether transfers will revert
     // So it's not necessary to implement.
-    //receive() external payable { revert(); }
+    // receive() external payable { revert(); }
 
+    /// @notice Get balance
+    /// @return The balance of the user
     function getBalance() public view returns (uint) {
         return balances[msg.sender];
     }
 
+    /// @notice Enroll a customer with the bank
+    /// @return The users enrolled status
     function enroll() public returns (bool) {
         enrolled[msg.sender] = true;
         emit LogEnrolled(msg.sender);
         return enrolled[msg.sender];
     }
 
+    /// @notice Deposit ether into bank
+    /// @return The balance of the user after the deposit is made
     function deposit() public payable returns (uint) {
-        require(enrolled[msg.sender], "User is not enrolled.");
+        require(enrolled[msg.sender], "Customer is not enrolled.");
         balances[msg.sender] += msg.value;
         emit LogDepositMade(msg.sender, balances[msg.sender]);
         return balances[msg.sender];
     }
 
+    /// @notice Withdraw ether from bank
+    /// @dev This does not return any excess ether sent to it
+    /// @param withdrawAmount amount you want to withdraw
+    /// @return The balance remaining for the user
     function withdraw(uint withdrawAmount) public returns (uint) {
-        require(balances[msg.sender] >= withdrawAmount, "User has insufficient funds.");
+        require(balances[msg.sender] >= withdrawAmount, "Customer has insufficient funds.");
         payable(msg.sender).transfer(withdrawAmount);
         balances[msg.sender] -= withdrawAmount;
         emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
